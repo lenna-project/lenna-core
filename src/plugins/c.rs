570adfe0a18b723d5222b517cb4f68c51aee62bc
Christian M <chriamue@gmail.com>
2021-06-25 13:56:58 +0200

    added author, title and version to c api

--- src/plugins/c.rs
+++ src/plugins/c.rs
@@ -3,21 +3,48 @@ macro_rules! export_c_plugin {
     ($processor:ident) => {
         #[doc(hidden)]
         #[no_mangle]
-        extern "C" fn id() -> *const std::os::raw::c_char {
+        extern "C" fn lenna_plugin_id() -> *const std::os::raw::c_char {
             let processor = $processor::default();
             std::ffi::CString::new(processor.id()).unwrap().into_raw()
         }
 
         #[doc(hidden)]
         #[no_mangle]
-        extern "C" fn name() -> *const std::os::raw::c_char {
+        extern "C" fn lenna_plugin_name() -> *const std::os::raw::c_char {
             let processor = $processor::default();
             std::ffi::CString::new(processor.name()).unwrap().into_raw()
         }
 
         #[doc(hidden)]
         #[no_mangle]
-        extern "C" fn description() -> *const std::os::raw::c_char {
+        extern "C" fn lenna_plugin_title() -> *const std::os::raw::c_char {
+            let processor = $processor::default();
+            std::ffi::CString::new(processor.title())
+                .unwrap()
+                .into_raw()
+        }
+
+        #[doc(hidden)]
+        #[no_mangle]
+        extern "C" fn lenna_plugin_version() -> *const std::os::raw::c_char {
+            let processor = $processor::default();
+            std::ffi::CString::new(processor.version())
+                .unwrap()
+                .into_raw()
+        }
+
+        #[doc(hidden)]
+        #[no_mangle]
+        extern "C" fn lenna_plugin_author() -> *const std::os::raw::c_char {
+            let processor = $processor::default();
+            std::ffi::CString::new(processor.author())
+                .unwrap()
+                .into_raw()
+        }
+
+        #[doc(hidden)]
+        #[no_mangle]
+        extern "C" fn lenna_plugin_description() -> *const std::os::raw::c_char {
             let processor = $processor::default();
             std::ffi::CString::new(processor.description())
                 .unwrap()
