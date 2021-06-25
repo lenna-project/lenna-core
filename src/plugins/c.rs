f09f4c55905c0d313024c564214b2c0a297fb730
Christian M <chriamue@gmail.com>
2021-06-25 09:13:17 +0200

    :sparkles: added export c plugin macro rule

--- src/plugins/c.rs
+++ src/plugins/c.rs
@@ -1,10 +1,40 @@
-use crate::core::processor::Processor;
-use crate::core::resize::Resize;
-use std::ffi::CString;
-use std::os::raw::c_char;
+#[macro_export]
+macro_rules! export_c_plugin {
+    ($processor:ident) => {
+        #[doc(hidden)]
+        #[no_mangle]
+        extern "C" fn id() -> *const std::os::raw::c_char {
+            let processor = $processor::default();
+            std::ffi::CString::new(processor.id()).unwrap().into_raw()
+        }
 
-#[doc(hidden)]
-#[no_mangle]
-extern "C" fn getName() -> *const c_char {
-    CString::new(Resize::default().name()).unwrap().into_raw()
+        #[doc(hidden)]
+        #[no_mangle]
+        extern "C" fn name() -> *const std::os::raw::c_char {
+            let processor = $processor::default();
+            std::ffi::CString::new(processor.name()).unwrap().into_raw()
+        }
+
+        #[doc(hidden)]
+        #[no_mangle]
+        extern "C" fn description() -> *const std::os::raw::c_char {
+            let processor = $processor::default();
+            std::ffi::CString::new(processor.description())
+                .unwrap()
+                .into_raw()
+        }
+    };
+}
+
+#[cfg(test)]
+mod tests {
+
+    #[test]
+    fn module() {
+        use crate::core::processor::Processor;
+        use crate::core::resize::Resize;
+        #[allow(non_camel_case_types)]
+        type resize = Resize;
+        export_c_plugin!(resize);
+    }
 }
