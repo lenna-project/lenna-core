b29b63dac18c35a8fe845bf776088c28c8b66bb3
chriamue <chriamue@users.noreply.github.com>
2022-07-10 12:05:27 +0000

    deploy: d4423c7cfca5afe7c227195cf7ae23dfe398dfe0

--- source-files.js
+++ source-files.js
@@ -1,3 +1,3 @@
-var N = null;var sourcesIndex = {};
+var sourcesIndex = {};
 sourcesIndex["lenna_core"] = {"name":"","dirs":[{"name":"core","files":["config.rs","mod.rs","pipeline.rs","pool.rs","processor.rs","resize.rs"]},{"name":"io","files":["mod.rs","read.rs","write.rs"]},{"name":"plugins","files":["c.rs","mod.rs"]}],"files":["lib.rs"]};
 createSourceSidebar();
